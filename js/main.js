43d4b65d8a751bfff183f3a81a5d5d1a3a190aa2
nicole <nikkihmltn1@yahoo.com>
2021-06-07 18:32:31 -0700

    add turnip and cucumber assigned helpers functions for inc and dec

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let turnips = 0
 let cucumber = 0
-let cursors = 0
-
+let helpers = 0
+let turnCursor = 0
+let cucumberCursor = 0
 
 function clickTurnip(num) {
     turnips += num
@@ -14,18 +15,53 @@ function clickCucumber(num) {
 }
 
 function buyCursor(){
-    let cursorCost = Math.floor(10 * Math.pow(2.1, cursors))
-    if (turnips >= cursorCost) {
-        cursors++
-        turnips = turnips - cursorCost
-        document.getElementById("cursors").textContent = cursors
+    let helperCost = Math.floor(10 * Math.pow(2.1, helpers))
+    if (turnips >= helperCost) {
+        helpers++
+        turnips = turnips - helperCost
+        document.getElementById("helpers").textContent = helpers
         document.getElementById("turnip-counter").textContent = turnips
     }
-    let nextCost = Math.floor(10 * Math.pow(2.1, cursors))
-    document.getElementById("cursor-cost").textContent = nextCost
+    let nextCost = Math.floor(10 * Math.pow(2.1, helpers))
+    document.getElementById("helper-cost").textContent = nextCost
+}
+
+function incTurnHelp(num) {
+    if (helpers >=1) {
+        turnCursor++
+        helpers--
+    }
+    document.getElementById("turnip-helper").textContent = turnCursor
+    document.getElementById("helpers").textContent = helpers
+}
+
+function decTurnHelp(num) {
+    if (turnCursor >=1) {
+        turnCursor--
+        helpers++
+    }
+    document.getElementById("turnip-helper").textContent = turnCursor
+    document.getElementById("helpers").textContent = helpers
+}
+function incCucumberHelp(num) {
+    if (helpers >=1) {
+        cucumberCursor++
+        helpers--
+    }
+    document.getElementById("cucumber-helper").textContent = cucumberCursor
+    document.getElementById("helpers").textContent = helpers
+}
+
+function decCucumberHelp(num) {
+    if (cucumberCursor >=1) {
+        cucumberCursor--
+        helpers++
+    }
+    document.getElementById("cucumber-helper").textContent = cucumberCursor
+    document.getElementById("helpers").textContent = helpers
 }
 
 window.setInterval(() => {
-    clickTurnip(cursors)
-    clickCucumber(cursors)
+    clickTurnip(turnCursor)
+    clickCucumber(cucumberCursor)
 }, 1000)
